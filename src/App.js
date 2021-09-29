613115171fce033706d22ffcf679d0c35beca9e8
Manju Goyal <manju6goyal@gmail.com>
2021-09-29 19:03:04 +0530

    Basic landing page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './css/App.css';
+
+import LandingPage from './components/landingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
